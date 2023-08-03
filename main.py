f6912073eafbd9c0b8c7515af6361c445c21e693
Devan Anjelito <anjelitodevan@gmail.com>
2023-08-03 18:58:43 +0700

    Count pixels.

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import numpy as np
 import os
 import config
 
-def check_parking_spot(pos_list, img):
-    """Check if a spot is empty or not, draw bbox."""
 
+def process_img(img):
     img_gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_blur= cv2.GaussianBlur(img_gray, (3, 3), 1)
     img_bin= cv2.adaptiveThreshold(
@@ -17,16 +16,37 @@ def check_parking_spot(pos_list, img):
         25, 
         16
     )
-    return img_bin
+    img_med= cv2.medianBlur(img_bin, 5)
+    img_dilate= cv2.dilate(img_med, np.ones((3, 3), np.uint8), 1)
+    return img_dilate
+
+
+def check_parking_spot(pos_list, input_img, output_img, put_text= True):
+    """Check if a spot is empty or not."""
+    for (x1, y1) in pos_list:
+        x2= x1 + config.DELTA_X
+        y2= y1 + config.DELTA_Y
+        spot= input_img[y1:y2, x1:x2]
+        count_nonzero= cv2.countNonZero(spot)
+        if put_text:
+            cv2.putText(output_img, 
+                        str(count_nonzero), 
+                        (x1+25, y1+25), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        0.75, 
+                        (255, 255, 255), 
+                        2
+            )
+
 
 def draw_rectangle(pos_list, img, color):
+    """Draw rectangle in image for every spot."""
     for (x1, y1) in pos_list:
         x2= x1 + config.DELTA_X
         y2= y1 + config.DELTA_Y
         cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)    
     
-        
-
+    
 # get video 
 cap= cv2.VideoCapture(config.VIDEO_PATH)
 
@@ -55,7 +75,8 @@ while ret:
 
     # detect parking spot
     if pos_list:
-        frame= check_parking_spot(pos_list, frame)
+        frame_processed= process_img(frame)
+        check_parking_spot(pos_list, frame_processed, frame)
 
     # draw rectangle
     # draw_rectangle(pos_list, frame, (255, 0, 0))
